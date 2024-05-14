a67201815d48595082f4c79976427c45ddcdfac8
Mehmet Üstek <mustek17@ku.edu.tr>
2024-05-14 14:16:55 +0100

    Directory Scanner

--- main.py
+++ main.py
@@ -2,24 +2,51 @@ from compose.compose_vulnerability_detection import get_compose_file_vulnerabili
 from dockerfileVulnerability.dockerfile_vulnerability_detection import get_dockerfile_vulnerabilities
 from utils.file_extension import get_file_extension, get_filename
 import argparse
+import os
+
 
 ## Run
 # Compose at local: python3 main.py -f compose.yaml
 # Dockerfile at local: python3 main.py -f Dockerfile
 
+def get_single_file_vulnerability(file_path):
+    if get_file_extension(file_path) == ".Dockerfile" or get_filename(file_path) == "Dockerfile" :
+        get_dockerfile_vulnerabilities(file_path)
+    else:
+        get_compose_file_vulnerabilities(file_path)
+
+
+def get_directory_file_vulnerability(directory_path):
+    # Get a list of all files in the directory
+    files = os.listdir(directory_path)
+
+    # Print the names of each file
+    for file in files:
+        file_absolute_path = os.path.join(directory_path, file)
+        print(file_absolute_path)
+        get_single_file_vulnerability(file_absolute_path)
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Process some file paths.")
-    parser.add_argument('-f', '--file', type=str, help='File path to process', required=True)
+    parser = argparse.ArgumentParser(description="AST-matching IaC configuration file vulnerability scanner", add_help=True)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f', '--file', type=str, help='Absolute file path to process IaC configuration file', required=False)
+    group.add_argument('-d', '--directory', type=str, help='Absolute directory path to process IaC configuration file', required=False)
     
     args = parser.parse_args()
 
     # Here, you can add the code to handle or process the file
-    print(f"The provided file path is: {args.file}")
-    iac_file_path = args.file
-    if get_file_extension(iac_file_path) == "Dockerfile" or get_filename(iac_file_path) == "Dockerfile" :
-        get_dockerfile_vulnerabilities(iac_file_path)
+    if args.directory:
+        print(f"The provided directory path is: {args.directory}")
+        directory_path = args.directory
+        get_directory_file_vulnerability(os.path.abspath(directory_path))
+    elif args.file: 
+        print(f"The provided file path is: {args.file}")
+        file_path = args.file
+        get_single_file_vulnerability(os.path.abspath(file_path))
     else:
-        get_compose_file_vulnerabilities(iac_file_path)
+        print("No file or directory specified.")
+    
 
 if __name__ == "__main__":
     main()
--- utils/logs/severity_printer.py
+++ utils/logs/severity_printer.py
@@ -1,7 +1,7 @@
 def print_severity(logger, severity, vulnerability):
     if severity == 10:
         logger.critical(vulnerability)
-    elif severity >= 7:
+    elif severity >= 6:
         logger.error(vulnerability)
-    elif severity < 7:
+    elif severity < 6:
         logger.warning(vulnerability)
