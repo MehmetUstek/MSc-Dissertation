566f558bbfd80ba693bfba9ee3f0744b4dfb0191
Mehmet Üstek <mustek17@ku.edu.tr>
2024-07-06 23:19:18 +0100

    Error Numbers for fastapi

--- fast.py
+++ fast.py
@@ -1,8 +1,9 @@
 import base64
-import json
 from fastapi import FastAPI, HTTPException
 from get_single_file_vulnerability import get_single_file_vulnerability_filecontent
 from pydantic import BaseModel
+
+from utils import load_json
 
 app = FastAPI()
 
@@ -12,10 +13,15 @@ class File(BaseModel):
 
 @app.post("/vulnerability_scan/")
 async def vulnerability_scan(file:File):
-    # TODO: Change the file extension.
     original_content = base64.b64decode(file.content.encode('utf-8')).decode('utf-8')
     print(original_content)
-    json_data = get_single_file_vulnerability_filecontent(file_content= original_content,file_extension="Dockerfile", baseImageScan=False)
-    print(json_data)
-    # json_payload = json.dumps(json_data)
+    print("filename", file.name)
+    json_data = get_single_file_vulnerability_filecontent(file_content= original_content,filename=file.name,
+                                                           baseImageScan=False)
+    error_descriptions = load_json('dockerfileVulnerability/antipattern_descriptions.json')
+    print("json_data",json_data)
+
+    for item in json_data:
+        print("item", item)
+        item["errorNo"] = error_descriptions[item["errorNo"]]
     return json_data
